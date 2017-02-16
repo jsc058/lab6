8616b389d9003dd74f69fecb1874f1adddfc78f7
Jennifer Chan <jsc058@ucsd.edu>
2017-02-16 09:57:09 -0800

    Lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,6 +17,10 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+ // $("testjs").click(function(e)) {}
+ // $.get("/project/", addProjectDetails);
+ // }
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -26,7 +30,26 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+  $.get("/project/" + idNumber, callBackFn);
+
 	console.log("User clicked on project " + idNumber);
+	console.log("URL is /project/" + idNumber);
+
+	$("#project" + idNumber).html
+
+}
+
+function callBackFn(result) {
+	console.log(result);
+
+	var detailHTML =
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p>' +
+		'<p><small>' + result['summary'] + '</small></p>';
+		//console.log(detailHTML);
+
+	$("#project" + result['id'] + " .details").html(detailHTML);
 }
 
 /*
@@ -35,4 +58,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", callBackFnColor);
+}
+
+function callBackFnColor(result) {
+	console.log(result);
+	var colors0 = result['colors'];
+	var colors = colors0.hex;
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
